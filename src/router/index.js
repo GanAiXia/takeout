73b3024b67117c5a4afa5f14440adc226efa0837
ganaixia <wanghuigan@126.com>
2019-12-31 14:47:32 +0800

    '商品页面添加以及路由、mock数据商品添加'

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,41 @@ import Order from '../pages/Order/Order.vue'
 import Search from '../pages/Search/Search.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import Login from '../pages/Login/Login.vue'
+import Shop from '../pages/Shop/Shop.vue'
+import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue'
+import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue'
+import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
     routes: [
-        {path: '/', component: Msite},
-        {path: '/msite', component: Msite},
-        {path: '/search', component: Search},
-        {path: '/order', component: Order},
-        {path: '/profile', component: Profile},
-        {path: '/login', component: Login}
+        {path: '/', component: Msite, meta: { showFooter: true }},
+        {path: '/msite', component: Msite, meta: { showFooter: true }},
+        {path: '/search', component: Search, meta: { showFooter: true }},
+        {path: '/order', component: Order, meta: { showFooter: true }},
+        {path: '/profile', component: Profile, meta: { showFooter: true }},
+        {path: '/login', component: Login},
+        {
+            path: '/shop', 
+            component: Shop, 
+            children: [
+                {
+                    path: '/shop/goods', 
+                    component: ShopGoods
+                },
+                {
+                    path: '/shop/info', 
+                    component: ShopInfo
+                },
+                {
+                    path: '/shop/ratings', 
+                    component: ShopRatings
+                },
+                {
+                    path: '', 
+                    component: ShopGoods
+                }
+            ]}
     ]
 })
